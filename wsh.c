88eeed6dfb71a9652486e699c474e53ac0e4e0b7
masonrware <86129546+masonrware@users.noreply.github.com>
2023-09-29 18:24:46 -0500

    implemented file search on path

--- wsh.c
+++ wsh.c
@@ -8,12 +8,12 @@
 #define PATH "/bin"
 
 // built-in commands
-void exit() {
+void wsh_exit() {
   exit(0);
 }
 
 void cd() {
-  // test comment for git
+
 }
 
 void jobs() {
@@ -28,7 +28,7 @@ void bg() {
 
 }
 
-
+// run function for interactive mode
 int runi() {
   while(true) {
     printf("wsh> ");
@@ -39,23 +39,56 @@ int runi() {
 
     // check if EOF is reached/input
     if(line == NULL){
-	  exit();
+	  wsh_exit();
     }
 
     // remove newline
     if (cmd[strlen(cmd)-1] == '\n')
       cmd[strlen(cmd)-1] = '\0';
     
-    // TODO:  potentially seperate out arguments provided (ls -la file)
-
-    printf("you typed: %s\n", cmd);
-    if(strcmp(cmd, "exit")==0) {
-      exit();
+    // TODO:  potentially separate out arguments provided (ls -la file) 
+    
+    // exit
+    if(strcmp(cmd, "exit") == 0){
+      wsh_exit();
+    } 
+    // cd
+    else if(strcmp(cmd, "cd") == 0) {
+      printf("Handle %s\n", cmd);
+    }
+    // jobs
+    else if(strcmp(cmd, "jobs") == 0) {
+      printf("Handle %s\n", cmd);
+    } 
+    // fg
+    else if(strcmp(cmd, "fg") == 0) {
+      printf("Handle %s\n", cmd);
+    } 
+    // bg
+    else if(strcmp(cmd, "bg") == 0) {
+      printf("Handle %s\n", cmd);
+    }
+    // search path
+    else {
+      char *path = strtok(PATH, ":");
+      while (path != NULL) {
+        strcat(path, "/");
+        strcat(path, cmd);
+        if (access(path, X_OK) != -1) {
+          printf("FOUND %s EXECUTABLE @ %s\n", cmd, path); 
+          break;
+        }
+        path = strtok(NULL, ":");
+      }
+      // didn't find .exe for cmd
+      printf("ERROR: could not find %s executable on provided path.\n", cmd);
+      wsh_exit();
     }
   }
   return 0;
 }
 
+// run function for batch mode
 int runb(char *batch_file) {
   // open batch file and iterate over it
   return 0;
